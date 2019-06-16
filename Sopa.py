b42761cfd151ca87d517e13959ff2d38f3c9eaf3
elvisdavid00 <51764212+elvisdavid00@users.noreply.github.com>
2019-06-16 00:07:16 -0300

    Add files via upload

--- Sopa.py
+++ Sopa.py
@@ -1,6 +1,7 @@
 import sys  
 import random
 import string
+import textwrap
 if sys.version_info[0] >= 3:  
     import PySimpleGUI as sg  
 else:  
@@ -11,12 +12,12 @@ def agregar_clase_palabra_horizontal (palabras_lista, cant_palabras, coord_palab
 	for i in range(cant_palabras):
 		fila = random.randrange(cant_filas)
 		while len(layout[fila]) > 0:
-			fila = random.randrange(cant_filas)
+			fila = random.randrange(cant_filas) 
 		letras_inicio = random.randrange(cant_columas - len(palabras_lista[i]))	
 		for j in range(letras_inicio):
-			layout[fila].append(sg.Button( random.choice(string.ascii_letters).upper(), key = (fila,j), font = 30, button_color = ("black", color_sin_marcar)))
+			layout[fila].append(sg.Button( random.choice(string.ascii_letters).upper(), key = (fila,j), font = 15, button_color = ("black", color_sin_marcar),pad = (0,0)))
 		for j in range(letras_inicio, letras_inicio + len(palabras_lista[i])):
-			layout[fila].append(sg.Button(palabras_lista[i][j-letras_inicio].upper(),key = (fila,j), font = 30, button_color = ("black", color_sin_marcar)))
+			layout[fila].append(sg.Button(palabras_lista[i][j-letras_inicio].upper(),key = (fila,j), font = 15, button_color = ("black", color_sin_marcar),pad = (0,0)))
 			coord_palabras.append((fila,j))
 
 def convertir_vertical (layout, cant_columnas, cant_filas):
@@ -28,29 +29,34 @@ def convertir_vertical (layout, cant_columnas, cant_filas):
 			layout_vertical[columna].append(layout[fila][columna])
 	return layout_vertical		
 
-
-#Opciones:
-
 #Programa Principal
-
-
-
-#definir Opciones
-
+wrapper = textwrap.TextWrapper(width=50) 
 diccionario_colores = {"color_sin_marcar": "", "color_marcado":"", "color_sustantivo":"","color_verbo":"","color_adjetivo":""}
 tipo_ayuda = {"mostrar_definicion": False, "mostrar_lista": False}
 orientacion_vertical =[]
 cant_palabras = {"cant_sustantivos":0,"cant_verbos":0,"cant_adjetivos":0}
 sopa_mayusculas = []
 paleta_colores = {"color_ventana":"","color_letras":"","color_botones":"","color_sopa":""}
+
 #invocacion a configuracion
 Configuracion_sopa.configurar_sopa(diccionario_colores, tipo_ayuda,orientacion_vertical,cant_palabras,sopa_mayusculas)
-
 diccionario_palabras = Configuracion_sopa.Ingreso_de_palabras ()		
 
-# ~ sustantivos = {"pelota": "objeto redondo", "celular": "medio de comunicacion", "teclado": "dispositivo para escribir", "lapicera" : "objeto usado para escribir"}
-# ~ verbos = {"hacer": "...", "tomar": "...", "jugar": "...", "hablar" : "..."}
-# ~ adjetivos = {"feo": "...", "rojizo": "...", "grande": "...", "colorido" : "..."}
+#Lista random de las palabras 
+cant_sustantivos =int(cant_palabras["cant_sustantivos"])
+if cant_sustantivos > len(diccionario_palabras["NN"]):
+	cant_sustantivos = len(diccionario_palabras["NN"])
+cant_verbos =int(cant_palabras["cant_verbos"])
+if cant_verbos > len(diccionario_palabras["VB"]):
+	cant_verbos = len(diccionario_palabras["VB"])
+cant_adjetivos = int(cant_palabras["cant_adjetivos"])
+if cant_adjetivos > len(diccionario_palabras["JJ"]):
+	cant_adjetivos = len(diccionario_palabras["JJ"])
+	
+sustantivos_lista = random.sample(list(diccionario_palabras["NN"].keys()),cant_sustantivos)
+verbos_lista = random.sample(list(diccionario_palabras["VB"].keys()),cant_verbos)
+adjetivos_lista = random.sample(list(diccionario_palabras["JJ"].keys()),cant_adjetivos)
+ 
 vertical = not orientacion_vertical[0]
 color_sin_marcar = "grey35"
 color_marcado_sustantivo = diccionario_colores["color_sustantivo"]
@@ -68,13 +74,6 @@ color_marcado_adjetivo = diccionario_colores["color_adjetivo"]
 	# ~ adjetivos_lista = list(adjetivos.keys())
 	# ~ lista_letras = lista_letras_mayusculas
 
-sustantivos_lista = list(diccionario_palabras["NN"].keys())
-verbos_lista = list(diccionario_palabras["VB"].keys())
-adjetivos_lista = list(diccionario_palabras["JJ"].keys())
-
-cant_sustantivos =int(cant_palabras["cant_sustantivos"]) 
-cant_adjetivos = int(cant_palabras["cant_adjetivos"])
-cant_verbos =int(cant_palabras["cant_verbos"])
 
 #Horizontal
 
@@ -89,7 +88,7 @@ coord_sustantivos =[]
 coord_verbos =[]
 coord_adjetivos =[]
 #Opciones para el estilo de la sopa de letras
-sg.SetOptions(element_padding=(0, 0),margins=(0,20), button_element_size=(6, 3), auto_size_buttons=False,background_color="grey20",text_color = "snow")	
+sg.SetOptions(button_element_size=(4, 2), auto_size_buttons=False,background_color="grey20")	
 for i in range (cant_filas):
 	row = []
 	layout_sopa.append(row)
@@ -107,7 +106,7 @@ for fila in range (cant_filas):
 		fila_llena = len(layout_sopa[fila])
 		letras_faltantes = cant_columas - len(layout_sopa[fila])
 		for j in range (letras_faltantes):
-			layout_sopa[fila].append(sg.Button( random.choice(string.ascii_letters).upper(), font = 30, key = (fila,j+fila_llena), button_color = ("black", color_sin_marcar)))
+			layout_sopa[fila].append(sg.Button( random.choice(string.ascii_letters).upper(), font = 15, key = (fila,j+fila_llena), button_color = ("black", color_sin_marcar),pad = (0,0)))
 
 if vertical:
 	layout_sopa = convertir_vertical(layout_sopa,cant_columas, cant_filas) 
@@ -116,14 +115,64 @@ sustantivos_marcados =[]
 verbos_marcados =[]
 adjetivos_marcados =[]
 
-columna1 = [[sg.Text('Opciones ',background_color ="grey20", text_color = "snow", justification='center', size=(10, 1))],      
-               [sg.Radio('Sustantivo', "clase_palabra",key = "sustantivo_radio",size=(10, 15),font = 25,text_color = "grey15" ,background_color = diccionario_colores["color_sustantivo"],		pad=(10,10), default=True )],
-               [sg.Radio('Verbo', "clase_palabra",key = "verbo_radio",			size=(10, 15),font = 25,text_color = "grey15" ,background_color = diccionario_colores["color_verbo"] ,pad=(10,10) )],
-               [sg.Radio('Adjetivo', "clase_palabra",key = "adjetivo_radio", 	size=(10, 15),font = 25,text_color = "grey15" ,background_color = diccionario_colores["color_adjetivo"],pad=(10,10) )],
-               [sg.Button("Comprobar", key = "comprobar_boton", button_color = ("black","LightSkyBlue2"),size=(20, 10),pad=(10,10) )],
-               [sg.Exit(button_color=("black","LightSkyBlue2"),pad=(60,150))]]
+#Lista de definiciones de palabras:
+lista_ayuda = []
+if tipo_ayuda["mostrar_definicion"] and not tipo_ayuda["mostrar_lista"]:
+	sustantivos_definiciones =[]
+	for i in sustantivos_lista:
+		sustantivos_definiciones.append([sg.Text("- "+ wrapper.fill(diccionario_palabras["NN"][i]))])
+	verbos_definiciones =[]
+	for i in verbos_lista:
+		verbos_definiciones.append([sg.Text("- "+ wrapper.fill(diccionario_palabras["VB"][i]))])
+	adjetivos_definiciones =[]
+	for i in adjetivos_lista:
+		adjetivos_definiciones.append([sg.Text("- "+ wrapper.fill(diccionario_palabras["JJ"][i]))])
+	lista_ayuda.append([sg.Frame("Sustantivos:",sustantivos_definiciones)])
+	lista_ayuda.append([sg.Frame("Verbos:",verbos_definiciones)])
+	lista_ayuda.append([sg.Frame("Adjetivos:",adjetivos_definiciones)])
+			
+if tipo_ayuda["mostrar_lista"] and not tipo_ayuda["mostrar_definicion"]:
+	sustantivos_ayuda =[]
+	for i in sustantivos_lista:
+		sustantivos_ayuda.append([sg.Text("- " + i).upper()])
+	verbos_ayuda =[]
+	for i in verbos_lista:
+		verbos_ayuda.append([sg.Text("- " + i).upper()])
+	adjetivos_ayuda =[]
+	for i in adjetivos_lista:
+		adjetivos_ayuda.append([sg.Text("- " + i).upper()])
+	lista_ayuda.append([sg.Frame("Sustantivos:",sustantivos_ayuda)])
+	lista_ayuda.append([sg.Frame("Verbos:",verbos_ayuda)])
+	lista_ayuda.append([sg.Frame("Adjetivos:",adjetivos_ayuda)])
+	
+if tipo_ayuda["mostrar_definicion"] and tipo_ayuda["mostrar_lista"]:
+	sustantivos_definiciones =[]
+	for i in sustantivos_lista:
+		sustantivos_definiciones.append([sg.Text("- " + wrapper.fill(i.upper() +": "+diccionario_palabras["NN"][i]))])
+	verbos_definiciones =[]
+	for i in verbos_lista:
+		verbos_definiciones.append([sg.Text("- " + wrapper.fill(i.upper()+": "+diccionario_palabras["VB"][i]))])
+	adjetivos_definiciones =[]
+	for i in adjetivos_lista:
+		adjetivos_definiciones.append([sg.Text("- " + wrapper.fill(i.upper()+": "+diccionario_palabras["JJ"][i]))])
+	
+	lista_ayuda.append([sg.Frame("Sustantivos:",sustantivos_definiciones)])
+	lista_ayuda.append([sg.Frame("Verbos:",verbos_definiciones)])
+	lista_ayuda.append([sg.Frame("Adjetivos:",adjetivos_definiciones)])
 
-layout = [[sg.Frame('Sopa De Letras', layout_sopa,background_color = "grey20"),sg.Column(columna1, background_color='grey20')]]
+if 	not tipo_ayuda["mostrar_definicion"] and not tipo_ayuda["mostrar_lista"]:
+	lista_ayuda.append([sg.Text("No selecciono ninguno de los modos de ayuda")])
+
+columna_ayuda = [[sg.Frame('Ayuda:',lista_ayuda)]]
+
+columna_seleccion_tipo = [[sg.Text('Opciones ',background_color ="grey20",  justification='center', size=(10, 1))],      
+               [sg.Radio('Sustantivo', "clase_palabra",key = "sustantivo_radio",size=(10, 15),font = 25,text_color = "grey15" ,background_color = diccionario_colores["color_sustantivo"], default=True )],
+               [sg.Radio('Verbo', "clase_palabra",key = "verbo_radio",			size=(10, 15),font = 25,text_color = "grey15" ,background_color = diccionario_colores["color_verbo"] )],
+               [sg.Radio('Adjetivo', "clase_palabra",key = "adjetivo_radio", 	size=(10, 15),font = 25,text_color = "grey15" ,background_color = diccionario_colores["color_adjetivo"] )],
+               [sg.Button("Comprobar", key = "comprobar_boton", button_color = ("black","LightSkyBlue2"),size=(20, 10),pad=(10,10) )],
+               [sg.Exit(button_color=("black","LightSkyBlue2"),pad=(60,50))]]
+
+layout = [[sg.Column(columna_ayuda),sg.Frame('Sopa De Letras', layout_sopa,background_color = "grey20"),sg.Column(columna_seleccion_tipo, background_color='grey20')]]
 
 
 window = sg.Window('Mi Sopa de Letras', layout)  
