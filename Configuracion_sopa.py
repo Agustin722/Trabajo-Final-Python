79538fa8c071ea970257e9778e5a6f12f68eeb16
Agustin722 <51764709+Agustin722@users.noreply.github.com>
2019-06-19 19:52:59 -0300

    cambio de imagen
    cambia como se ven las ventanas y la cantidad de sust/verb/adj por defecto

--- Configuracion_sopa.py
+++ Configuracion_sopa.py
@@ -11,38 +11,18 @@ def Ingreso_de_palabras():
 	
 	from pattern.web import Wiktionary
 	from pattern.es import parse, split, singularize
-	import os
 	import PySimpleGUI as sg
-	
-	def asignar_tipo(titulo, sustantivos, verbos, adjetivos):
-		# ~ Recibe el titulo de el objeto WiktionarySection y
-		# ~ devuelve el tipo de palabra que es la palabra 
-		# ~ ingresada, de acuerdo a ese titulo y actualiza
-		# ~ la lista de ese tipo para que lo muestre en la ventana
-		
-		if titulo.startswith('Sustantivo'):
-			tipo= 'NN'
-			if not(palabra in sustantivos):
-				sustantivos.append(palabra)
-		elif titulo.startswith('Verbo') or 'verbal' in titulo:
-			tipo= 'VB'
-			if not(palabra in verbos):
-				verbos.append(palabra)
-		elif titulo.startswith('Adjetivo') or 'adjetiva' in titulo:
-			tipo= 'JJ'
-			if not(palabra in adjetivos):
-				adjetivos.append(palabra)
-		return tipo
-						
+	sg.SetOptions(button_element_size=(4, 2), background_color = "grey20")
 	wiki= Wiktionary(language= "es")
 	dic= {'NN':{},'VB':{},'JJ':{}}
-	disenio1 =[[sg.Text('Ingrese un sustantivo, verbo o adjetivo para agregar a las palabras de la sopa de letras'), sg.InputText(do_not_clear=False, key= 'palabra')],
-			[sg.Output(size=(115,3), key= 'muestra')],
-			[sg.Button("Agregar"), sg.Button("Listo")]]
+	disenio1 =[[sg.Text('Ingrese un sustantivo, verbo o adjetivo para \nagregar a las palabras de la sopa de letras',background_color = "grey20",text_color = "snow"), sg.InputText(do_not_clear=False, key= 'palabra',background_color = "grey35",text_color = "snow",size=(35,1))],
+			[sg.T(" ", background_color = "grey20" )],
+			[sg.Output(size=(70,3), key= 'muestra',background_color = "grey35",text_color = "snow")],
+			[sg.T(" ", background_color = "grey20" )],
+			[sg.T(" "*20, background_color = "grey20" ), sg.Button("Agregar", size =(10,2) , button_color = ("#ffffff","#3698A9")), sg.T(" "*25, background_color = "grey20"), sg.Button("Listo",size =(10,2) , button_color = ("#ffffff","#3698A9"))]]
 	ventana1= sg.Window('Ingreso de palabras').Layout(disenio1)
-	if not('reporte.txt' in os.listdir()):
-		arch= open('reporte.txt','x')
-		arch.close()
+	arch= open('reporte.txt','w')
+	arch.close()
 	sustantivos= []
 	verbos= []
 	adjetivos= []
@@ -54,7 +34,7 @@ def Ingreso_de_palabras():
 			if(len(sustantivos) != 0 or len(verbos) != 0 or len(adjetivos) != 0):
 				break
 			else:
-				sg.PopupOK('Error, Ingrese al menos una palabra de los tipos pedidos',grab_anywhere=True)
+				sg.PopupOK('Error 420, Ingrese al menos una palabra de los tipos pedidos',grab_anywhere=True)
 		else:
 			palabra= values1['palabra']
 			if palabra != '' and not palabra.startswith(' ') and not palabra.endswith(' '):
@@ -68,20 +48,33 @@ def Ingreso_de_palabras():
 						pos= pos + 1
 					seccion= secciones[pos]
 					lista_seccion= seccion.string.split('\n')
-					pos= 0
-					tipo= asignar_tipo(seccion.title, sustantivos, verbos, adjetivos)
+					if seccion.title.startswith('Sustantivo'):
+						tipo= 'NN'
+						if not(palabra in sustantivos):
+							sustantivos.append(palabra)
+						pos= 7
+					elif seccion.title.startswith('Verbo') or 'verbal' in seccion.title:
+						tipo= 'VB'
+						if not(palabra in verbos):
+							verbos.append(palabra)
+						pos= 2
+					elif seccion.title.startswith('Adjetivo') or 'adjetiva' in seccion.title:
+						tipo= 'JJ'
+						if not(palabra in adjetivos):
+							adjetivos.append(palabra)
+						pos= 10 if seccion.title.startswith('Adjetivo') else 2
 					#Muestra que palabras se han ingresado hasta ahora
 					ventana1.Element('muestra').Update('Sustantivos: '+ str(sustantivos) +'\n'+'Verbos: '+str(verbos)+'\n'+'Adjetivos: '+str(adjetivos))
 					#Se busca la 1era definicion de la palabra
 					while(not lista_seccion[pos].startswith('1')):
 						pos= pos + 1
 					definicion= lista_seccion[pos][1:]
-					#Agrega en el reporte si el tipo de palabra que indican pattern y wikcionary difiere
 					if tipo != parse(palabra).split('/')[1]:
 						reporte= 'El tipo de '+ palabra +' que indica Wikcionario no coincide con el tipo que indica pattern'
 						arch= open('reporte.txt','a')
 						arch.write('-'+reporte+'\n')
 						arch.close()
+					dic[tipo][palabra]= definicion
 				except AttributeError:
 					reporte= palabra +' no se encuentra en Wikcionario'
 					arch= open('reporte.txt','a')
@@ -104,42 +97,47 @@ def Ingreso_de_palabras():
 					ventana2= sg.Window('Ingreso de definicion').Layout(disenio2)
 					boton, values2= ventana2.Read()
 					definicion= values2['definicion']
+					dic[tipo][palabra]= definicion
 					ventana2.Close()
-				dic[tipo][palabra]= definicion
 			else:
-				sg.PopupOK('Error, Ingrese la palabra otra vez',grab_anywhere=True)
+				sg.PopupOK('Error 722, Ingrese la palabra otra vez',grab_anywhere=True)
 	ventana1.Close()
 	return dic
 	
-def configurar_sopa(diccionario_colores, tipo_ayuda,orientacion_vertical,cant_palabras,sopa_mayusculas):
+def configurar_sopa(diccionario_colores, tipo_ayuda,orientacion_vertical,cant_palabras,sopa_mayusculas,cant_palabras_default):
 	# Establece la configuracion de la sopa de letras
 	# devolviendo los valores ingresados en estructuras de datos.
 	# En caso de que los colores se repitan se abre una ventana para avisar de esto.
 	import PySimpleGUI as sg
-	dic_colores= {'Rojo':'#f94a4f','Naranja':'#f29646','Plateado':'silver','Amarillo':'#f9e54a','Verde':'green','Cyan':'#2dc7e2','Azul':'#478de8','Purpura':'#9453c6','Magenta':'#dc6be0','Rosa':'#f78383'}
-	colores_español= list(dic_colores.keys())
+	sg.SetOptions(button_element_size=(4, 2), background_color = "grey20")
+	dic= {'Rojo':'#f94a4f','Naranja':'#f29646','Plateado':'silver','Amarillo':'#f9e54a','Verde':'green','Cyan':'#2dc7e2','Azul':'#478de8','Purpura':'#9453c6','Magenta':'#dc6be0','Rosa':'#f78383'}
 	
-	elegir_color = [[sg.Text('Ingrese un color para los sustantivos'), sg.InputCombo(colores_español, default_value='Rojo', key= 'color_sustantivo')],
-					[sg.Text('Ingrese un color para los verbos'), sg.InputCombo(colores_español, default_value='Amarillo', key= 'color_verbo')],
-					[sg.Text('Ingrese un color para los adjetivos'), sg.InputCombo(colores_español, default_value='Verde', key= 'color_adjetivo')]]
+	elegir_color = [[sg.Text('Elija un color para los sustantivos',background_color = "grey20",text_color = "snow"), sg.InputCombo(['Rojo','Naranja','Plateado','Amarillo','Verde','Cyan','Azul','Purpura','Magenta','Rosa'],default_value='Rojo', key= 'color_sustantivo',background_color = "grey20",text_color = "snow")],
+					[sg.Text('Elija un color para los verbos',background_color = "grey20",text_color = "snow"), sg.InputCombo(['Rojo','Naranja','Plateado','Amarillo','Verde','Cyan','Azul','Purpura','Magenta','Rosa'],default_value='Amarillo', key= 'color_verbo',background_color = "grey20",text_color = "snow")],
+					[sg.Text('Elija un color para los adjetivos',background_color = "grey20",text_color = "snow"), sg.InputCombo(['Rojo','Naranja','Plateado','Amarillo','Verde','Cyan','Azul','Purpura','Magenta','Rosa'],default_value='Verde', key= 'color_adjetivo',background_color = "grey20",text_color = "snow")]]
 	
-	elegir_ayuda = [[sg.Checkbox('Mostrar las definiciones de las palabras',key= 'mostrar_def')], 
-					[sg.Checkbox('Mostrar la lista de palabras a buscar',key= 'mostrar_lista')]]
+	elegir_ayuda = [[sg.Checkbox('Mostrar las definiciones de las palabras',key= 'mostrar_def',background_color = "grey20",text_color = "snow")], 
+					[sg.Checkbox('Mostrar la lista de palabras a buscar',key= 'mostrar_lista',background_color = "grey20",text_color = "snow")]]
 	
-	elegir_orientacion = [[sg.Radio('Horizontal', "RADIO1",default=True,key= 'hor'), sg.Radio('Vertical', "RADIO1",key= 'vert')]]
+	elegir_orientacion = [[sg.Radio('Horizontal', "RADIO1",default=True,key= 'hor',background_color = "grey20",text_color = "snow"), sg.Radio('Vertical', "RADIO1",key= 'vert',background_color = "grey20",text_color = "snow")]]
 	
-	elegir_cant_palabras = [[sg.Text('Sustantivos'), sg.Input(1,key= 'cant_sus')],
-							[sg.Text('Verbos'), sg.Input(1,key= 'cant_ver')],
-							[sg.Text('Adjetivos'), sg.Input(1,key= 'cant_adj')]]
+	elegir_cant_palabras = [[sg.Text('Sustantivos',background_color = "grey20",text_color = "snow"), sg.Input(cant_palabras_default[0],key= 'cant_sus',background_color = "grey35",text_color = "snow",size =(3,1))],
+							[sg.Text('Verbos      ',background_color = "grey20",text_color = "snow"), sg.Input(cant_palabras_default[1],key= 'cant_ver',background_color = "grey35",text_color = "snow",size =(3,1))],
+							[sg.Text('Adjetivos   ',background_color = "grey20",text_color = "snow"), sg.Input(cant_palabras_default[2],key= 'cant_adj',background_color = "grey35",text_color = "snow",size =(3,1))]]
 	
-	elegir_mayus_minus = [[sg.Radio('Mayuscula', "RADIO2",default=True,key= 'mayus'), sg.Radio('Minuscula', "RADIO2",key= 'minus')]]
+	elegir_mayus_minus = [[sg.Radio('Mayuscula', "RADIO2",default=True,key= 'mayus',background_color = "grey20",text_color = "snow"), sg.Radio('Minuscula', "RADIO2",key= 'minus',background_color = "grey20",text_color = "snow")]]
 	
-	configuracion= [[sg.Frame('Colores', elegir_color)],
-					[sg.Frame('Tipo de ayuda', elegir_ayuda)],
-					[sg.Frame('Orientacion de las palabras', elegir_orientacion)],
-					[sg.Frame('Cantidad a mostrar', elegir_cant_palabras)],
-					[sg.Frame('Tipo de letra de la sopa de letras', elegir_mayus_minus)],
-					[sg.Button("Listo")]]
+	configuracion= [[sg.Frame('Colores', elegir_color, background_color = "grey20",title_color = "snow")],
+					[sg.T(" ", background_color = "grey20" )],
+					[sg.Frame('Tipo de ayuda', elegir_ayuda, background_color = "grey20",title_color = "snow")],
+					[sg.T(" ", background_color = "grey20" )],
+					[sg.Frame('Orientacion de las palabras', elegir_orientacion, background_color = "grey20",title_color = "snow")],
+					[sg.T(" ", background_color = "grey20" )],
+					[sg.Frame('Cantidad a mostrar', elegir_cant_palabras, background_color = "grey20",title_color = "snow")],
+					[sg.T(" ", background_color = "grey20" )],
+					[sg.Frame('Tipo de letra de la sopa de letras', elegir_mayus_minus, background_color = "grey20",title_color = "snow")],
+					[sg.T(" ",background_color = "grey20")],
+					[sg.T(" "*25,background_color = "grey20"), sg.Button("Listo",size =(10,2) , button_color = ("#ffffff","#3698A9"))]]
 	ventana= sg.Window('Configuracion',resizable=True).Layout(configuracion)
 	while True:
 		boton, values= ventana.Read()
@@ -152,10 +150,9 @@ def configurar_sopa(diccionario_colores, tipo_ayuda,orientacion_vertical,cant_pa
 			else:
 				sg.PopupOK('Los colores de las palabras deben ser distintos!',grab_anywhere=True)
 	ventana.Close()
-	#Asignacion de los valores que se devuelven al programa principal
-	diccionario_colores["color_sustantivo"]= dic_colores[values['color_sustantivo']]
-	diccionario_colores["color_verbo"]= dic_colores[values['color_verbo']]
-	diccionario_colores["color_adjetivo"]= dic_colores[values['color_adjetivo']]
+	diccionario_colores["color_sustantivo"]= dic[values['color_sustantivo']]
+	diccionario_colores["color_verbo"]= dic[values['color_verbo']]
+	diccionario_colores["color_adjetivo"]= dic[values['color_adjetivo']]
 	tipo_ayuda["mostrar_definicion"]= values['mostrar_def']
 	tipo_ayuda["mostrar_lista"]= values['mostrar_lista']
 	orientacion_vertical.append(values["hor"])
