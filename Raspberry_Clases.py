4f9c49bcde51a786a8b29a52ba74c7a392fa6342
Agustin722 <51764709+Agustin722@users.noreply.github.com>
2019-07-10 17:30:52 -0300

    Add files via upload

---
+++ Raspberry_Clases.py
@@ -0,0 +1,42 @@
+import Adafruit_DHT
+import time
+
+class Temperatura:
+    def __init__(self, pin=17, sensor= Adafruit_DHT.DHT12):
+        self._sensor = sensor
+        self._data_pin = pin
+
+    def datos_sensor(self):
+      humedad, temperatura = Adafruit_DHT.read_retry(self._sensor, self._data_pin)
+      return {'temperatura': temperatura, 'humedad': humedad, 'fecha': time.asctime(time.localtime(time.time()))}
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+class Matriz:
+    def __init__(self, numero_matrices=2, orientacion=0, rotacion=0, ancho=8, alto=8):
+        self.font = [CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
+        self.serial = spi(port=0, device=0, gpio=noop())
+        self.device = max7219(self.serial, width= ancho, height= alto, cascaded= numero_matrices, rotate= rotacion)
+    
+    def mostrar_mensaje(self, msg, delay=0.1, font=1):
+        show_message(self.device, msg, fill= "white",font= proportional(self.font[font]),scroll_delay= delay)
+
+import RPi.GPIO as GPIO
+
+class Sonido:   
+    def __init__(self, canal=22):
+        self._canal = canal
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self._canal, GPIO.IN)
+        # Desactivo las warnings por tener más de un circuito en la GPIO
+        GPIO.setwarnings(False)
+        GPIO.add_event_detect(self._canal, GPIO.RISING)
+        
+    def evento_detectado(self, funcion):
+        if GPIO.event_detected(self._canal):
+            funcion()
